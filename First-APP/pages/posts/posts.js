1fcd851c44edcbca69c6da8ec52d81acc8aea4e5
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-02-18 23:55:24 +0800

    路由

--- First-APP/pages/posts/posts.js
+++ First-APP/pages/posts/posts.js
@@ -12,7 +12,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+      let posts = [{
+        date: "Sep 18 2016",
+        title: "正是虾肥蟹壮时",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/1.png",
+        content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+        reading: "112",
+        collection: "98"
+      },{
+        date: "Sep 18 2016",
+        title: "正是虾肥蟹壮时",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/1.png",
+        content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+        reading: "112",
+        collection: "98"
+      }];
 
+      this.setData({posts})
   },
 
   /**
--- First-APP/pages/welcome/welcome.js
+++ First-APP/pages/welcome/welcome.js
@@ -1,3 +1,14 @@
 Page({
-  
+  onTapFunc: function() {
+   wx.navigateTo({
+     url: '../posts/posts'
+   })
+  },
+
+  onUnload: function(){
+    console.log("unload");
+  },
+  onHide: function(){
+    console.log("onHide");
+  }
 })
