3dfd1c0f3ed99150bb572c7dc2253e6a33aa6a34
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-02-21 17:17:47 +0800

    movie组件

---
+++ SecondApp/components/classic/movie/index.js
@@ -0,0 +1,24 @@
+// components/classic/movie/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    image:String,
+    content:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tagImg: './images/movie@tag.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
