f7657f7f2dcdbf3fc720c7f4abc85c568e8f1675
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-02-21 22:32:53 +0800

    nav 导航

---
+++ SecondApp/components/navi/index.js
@@ -0,0 +1,28 @@
+// components/nav/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:String,
+    first: Boolean,
+    last: Boolean
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    leftImg: "./images/triangle@left.png",
+    leftDisImg: "./images/triangle.dis@left.png",
+    rightImg: "./images/triangle@right.png",
+    rightDisImg: "./images/triangle.dis@right.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
