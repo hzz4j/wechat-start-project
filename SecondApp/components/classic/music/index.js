4d7738966cc6ce90a5a70e7b80f830764d486568
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-02-22 18:45:01 +0800

    控制音乐播放

--- SecondApp/components/classic/music/index.js
+++ SecondApp/components/classic/music/index.js
@@ -1,10 +1,14 @@
 import {classcicBehavior} from '../classic-behavior';
+const audioMgr = wx.getBackgroundAudioManager();
 Component({
   behaviors: [classcicBehavior],
   /**
    * 组件的属性列表
    */
-  properties: {},
+  properties: {
+    src:String,
+    title:String
+  },
 
   /**
    * 组件的初始数据
@@ -12,13 +16,29 @@ Component({
   data: {
     pausePlayerImg: "./images/player@pause.png",
     playPlayerImg: "./images/player@play.png",
-    musicTagImg: "./images/music@tag.png"
+    musicTagImg: "./images/music@tag.png",
+    playing: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onPlay(event){
+      this.setData({
+        playing: true
+      })
 
+      audioMgr.src = this.properties.src;
+      audioMgr.title = this.properties.title;
+
+    },
+
+    onPause(event){
+      this.setData({
+        playing: false
+      })
+      audioMgr.pause();
+    }
   }
 })
